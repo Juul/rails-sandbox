c826d4ed24aafdbc8c10893759ff02958963a8dc
Marc Juul Christoffersen <marcjc@gmail.com>
2012-04-12 13:25:48 -0700

    added database yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,51 @@
-set :application, "foo"
-set :repository,  "git://github.com/BIOFAB/biofab-web.git"
+require "bundler/capistrano"
 
-set :scm, :git
+server "biofab.ven.li", :web, :app, :db, :primary => true
 
-set :deploy_to, "/var/rails/foo"
+set :application, "rails-sandbox"
+set :user, "deployer"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
 
-role :web, "biofab.jbei.org"                          # Your HTTP server, Apache/etc
-role :app, "biofab.jbei.org"                          # This may be the same as your `Web` server
-role :db,  "biofab.jbei.org", :primary => true        # This is where Rails migrations will run
+set :scm, "git"
+set :repository, "git@github.com:Juul/#{application}.git"
+set :branch, "master"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, :roles => :app, :except => {:no_release => true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
+
+  task :setup_config, :roles => :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, :roles => :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, :roles => :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+end
