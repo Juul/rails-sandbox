dc3a4cdf3625b12b659a30c0188b0a7a471ade55
Marc Juul Christoffersen <marcjc@gmail.com>
2012-04-12 17:19:04 -0700

    better unicorn configuration

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,6 +5,17 @@ stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
 listen "/tmp/unicorn.blog.sock"
-worker_processes 2
+worker_processes 3
 timeout 30
 
+preload_app true
+
+before_fork do |server, worker|
+  # prevent master process from holding db connection
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  # re-establish connection after forking
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
