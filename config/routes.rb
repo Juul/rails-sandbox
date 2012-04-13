f7e76e6ad9719f501f237968424976dc7266e2fd
Marc Juul Christoffersen <marcjc@gmail.com>
2012-04-12 17:00:32 -0700

    added some scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Foo::Application.routes.draw do
+  resources :posts
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -46,9 +48,7 @@ Foo::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'posts#index'
 
   # See how all your routes lay out with "rake routes"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120412235338) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "topic"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
