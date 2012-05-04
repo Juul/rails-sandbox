a293e3809cdf85927d86fd204c5f1e56f6e1d34c
Marc Juul Christoffersen <marcjc@gmail.com>
2012-05-03 22:18:35 -0700

    production biofab database

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require "bundler/capistrano"
 
-server "biofab.ven.li", :web, :app, :db, :primary => true
+server "root@biofab.ven.li", :web, :app, :db, :primary => true
 
 set :application, "rails-sandbox"
 set :user, "deployer"
@@ -26,8 +26,8 @@ namespace :deploy do
   end
 
   task :setup_config, :roles => :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+#    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+#    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
     put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
     puts "Now edit the config files in #{shared_path}."
@@ -41,7 +41,7 @@ namespace :deploy do
 
   desc "Make sure local git is in sync with remote."
   task :check_revision, :roles => :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+    if `git rev-parse HEAD` != `git rev-parse origin/master`
       puts "WARNING: HEAD is not the same as origin/master"
       puts "Run `git push` to sync changes."
       exit
